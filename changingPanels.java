493554c9b6b58689f00170e004a4136636ea4b73
Zachary Chucka <38301483+zchucka@users.noreply.github.com>
2018-04-28 18:55:18 -0700

    Add files via upload

---
+++ changingPanels.java
@@ -0,0 +1,53 @@
+import java.awt.*;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import java.awt.event.*;
+
+public class changingPanels {
+	public static void main(String[] args) { 
+		JFrame frame = new JFrame("Learning how to change panels");
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500, 500);
+		
+		JPanel firstPanel = new JPanel();
+		JLabel firstLabel = new JLabel("The first Panel");
+		firstLabel.setVisible(true);
+		JButton firstButton = new JButton("Click to Change Panel");
+		firstPanel.add(firstLabel);
+		firstPanel.add(firstButton);
+		
+		frame.add(firstPanel);
+		
+		JPanel secondPanel = new JPanel();
+		JLabel secondLabel = new JLabel("The second Panel");
+		secondPanel.add(secondLabel);
+		firstPanel.setVisible(true);
+		
+		panelAction action = new panelAction(secondPanel, frame);
+		firstButton.addActionListener(action);
+		
+	}
+	
+	static private class panelAction implements ActionListener
+	{
+		JPanel panelHelper;
+		JFrame frameHelper;
+		
+		private panelAction(JPanel panel, JFrame frame)
+		{
+			panelHelper = panel;
+			frameHelper = frame;
+		}
+		
+		public void actionPerformed(ActionEvent event)
+		{
+			frameHelper.setContentPane(panelHelper);
+			frameHelper.pack();
+		}
+	}
+}
