fb4c5f06f7453242f153a9037e243a5e538f8399
ohassebrock <38301636+ohassebrock@users.noreply.github.com>
2018-04-30 21:12:42 -0700

    player round 2

--- player.java
+++ player.java
@@ -1,30 +1,39 @@
+
 /**
  * Class player for Farkle.
  * This class handles tracking score
- * 
+ *
  *      Methods: add points
- * 
+ *
  * CPSC224-01 Spring 2018
- * 
+ *
  * @Authors Bo Volwiler
  *          Zach Chucka
  *          Olivia Hassebrock
  * @Version V1 3/7/18
- * 
+ *
  */
 
-public class player
+public class player implements Cloneable
 {
     private int points = 0;
-    private String playerName;
+    private int playerName;
+
+
+    /**
+     * constructor for object player
+     */
+    public player() {
+        points = 0;
+    }
 
     /**
      * Adds points to the player's score
-     * 
-     * @param the points to be added
-     * 
+     *
+     * @parameter the points to be added
+     *
      * @returns
-     * 
+     *
      */
     public void addPoints(int toAdd)
     {
@@ -33,33 +42,50 @@ public class player
 
     /**
      * Sets the player's name
-     * 
-     * @param the name name of the player
-     * 
+     *
+     * @parameter the name name of the player
+     *
      * @returns
      */
-    public void setName(String name)
+    public void setName(int name)
     {
         playerName = name;
     }
 
     /**
      * Return's the player's name
-     * 
+     *
      * @param
-     * 
+     *
      * @returns the player's name
      */
-    public String getName()
+    public int getName()
     {
         return playerName;
     }
-    
+
     /*
      * returns the players score
      */
     public int getScore()
     {
-    	return points;
+        return points;
+    }
+
+    /*
+     * creates a deep copy of the player
+     * @param none
+     * @returns a deep copy of the player
+     * @throw CloneNotSupportedException if the fields of dice are not cloneable
+     */
+    public player clone()
+    {
+        try {
+            return (player) super.clone();
+        } catch (CloneNotSupportedException e)
+        {
+            return null;
+        }
+
     }
 }
