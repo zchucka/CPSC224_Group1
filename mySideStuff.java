229d586920363406456f75ad3b9a960b557ca041
Zachary Chucka <38301483+zchucka@users.noreply.github.com>
2018-04-30 21:13:39 -0700

    Add files via upload

--- mySideStuff.java
+++ mySideStuff.java
@@ -11,8 +11,11 @@ public class mySideStuff {
 	static hand gameHand = new hand(6);
 	static JPanel buttPanel = new JPanel();
 	static JPanel firstPanel = new JPanel();
+	static JPanel directionPanel = new JPanel();
 	static JFrame frame = new JFrame("Button Play");
 	static int playerNumber = 1;
+	static int roundScore = 0;
+	static JPanel playPanel = new JPanel();
 	
 	public static void main(String[] args) { 
 		frame.setVisible(true);
@@ -28,6 +31,7 @@ public class mySideStuff {
 		JLabel divider = new JLabel("-----------------------------");
 		scoreBoard.add(header);
 		scoreBoard.add(divider);
+		
 		for (int k = 0; k < 4; k++)
 		{
 			JLabel temp = new JLabel("Player " + (k+1) + ": 2000");
@@ -45,10 +49,11 @@ public class mySideStuff {
 		
 		buttPanel.setLayout(new BoxLayout(buttPanel, BoxLayout.LINE_AXIS));
 		
+		System.out.println(gameHand.numOfDice);
 		gameHand.roll();
 		
 		// creating the dice
-		for (int k = 0; k < 6; k++)
+		for (int k = 0; k < gameHand.numOfDice; k++)
 		{
 			JButton helper = new JButton("  " + gameHand.displayTheDiceValue(k) + "  ");
 			helper.addActionListener(new ColorAction(helper, k));
@@ -59,12 +64,10 @@ public class mySideStuff {
 		JButton endTurn = new JButton("End Turn");
 		
 		// making end turn button and keep playing button
-		JPanel playPanel = new JPanel();
 		playPanel.add(play);
 		playPanel.add(endTurn);
 		
 		// making instruction
-		JPanel directionPanel = new JPanel();
 		JLabel direction = new JLabel("Select which dice you want to keep");
 		directionPanel.add(direction);
 		
@@ -97,15 +100,10 @@ public class mySideStuff {
 		panelAction player = new panelAction();
 		endTurnAction endIt = new endTurnAction();
 		
-		/*
-		ColorAction yellowAction = new ColorAction(Color.yellow);
-        ColorAction blueAction = new ColorAction(Color.blue);
-        ColorAction redAction = new ColorAction(Color.red); */
-		
 		// associating listeners
 		play.addActionListener(player);
 		endTurn.addActionListener(endIt);
-		
+		frame.setVisible(true);
 		}
 	
 	
@@ -141,31 +139,37 @@ public class mySideStuff {
     
     static private class panelAction implements ActionListener
 	{
-		int numOfRemove = 0;
 		Boolean[] hasBeenRemoved = new Boolean[6];
 		
 		private panelAction()
 		{
-			resetBool();
+			for (int k = 0; k < 6; k++)
+			{
+				hasBeenRemoved[k] = false;
+				gameHand.setRolling(k, true);
+			}
 		}
 		
 		private void resetBool()
 		{
+			gameHand = new hand(6);
+			gameHand.roll();
+			System.out.println(gameHand.numOfDice);
 			for (int k = 0; k < 6; k++)
 			{
-				gameHand.setRolling(k, true);
 				hasBeenRemoved[k] = false;
+				gameHand.setRolling(k, true);
 			}
-			if (numOfRemove == 6)
+			
+			gameHand.roll();
+			
+			for (int k = 0; k < 6; k++)
 			{
-				for (int k = 0; k < 6; k++)
-				{
-					JButton helper = new JButton("  " + gameHand.displayTheDiceValue(k) + "  ");
-					helper.addActionListener(new ColorAction(helper, k));
-					buttPanel.add(helper);
-				}
-				gameHand.roll();
+				JButton helper = new JButton("  " + gameHand.displayTheDiceValue(k) + "  ");
+				helper.addActionListener(new ColorAction(helper, k));
+				buttPanel.add(helper);
 			}
+			
 			frame.setVisible(true);
 		}
 		
@@ -173,22 +177,37 @@ public class mySideStuff {
 		{
 			buttPanel.removeAll();
 			buttPanel.repaint();
-			int count = 0;
-			for (int k = 0; k < 6; k++)
+			
+			// send hand to score
+			
+			gameHand = new hand(true, gameHand);
+			System.out.println(gameHand.numOfDice);
+			gameHand.roll();
+			gameHand.displayRoll();
+			
+			for (int k = 0; k < gameHand.numOfDice; k++)
 			{
-				if(mySideStuff.gameHand.cupOfDice[k].moreRolling() == true)
-				{
-					JButton helper = new JButton("  " + gameHand.displayTheDiceValue(k) + "  ");
-					helper.addActionListener(new ColorAction(helper, k));
-					buttPanel.add(helper);
-					count++;
-				}
+				JButton helper = new JButton("  " + gameHand.displayTheDiceValue(k) + "  ");
+				helper.addActionListener(new ColorAction(helper, k));
+				buttPanel.add(helper);
 			}
-			numOfRemove = 6 - count;
-			if (numOfRemove == 6)
+			
+			if (false) // add that check validity shit
+			{
+				buttPanel.removeAll();
+				JLabel farkle = new JLabel("Get Farked Up");
+				buttPanel.add(farkle);
+				roundScore = 0;
+				buttPanel.repaint();
+				buttPanel.setVisible(true);
+				frame.setVisible(true);
+				endTurnAction.isFarkle(true);
+				playPanel.remove(0);
+			}
+			
+			if (gameHand.numOfDice == 0)
 			{
 				resetBool();
-				numOfRemove = 0;
 			}
 			buttPanel.setLayout(new BoxLayout(buttPanel, BoxLayout.LINE_AXIS));
 			buttPanel.repaint();
@@ -199,12 +218,15 @@ public class mySideStuff {
 	}
     
     static private class endTurnAction implements ActionListener {
+    	static Boolean didIFarkle = false;
     	
     	public void actionPerformed(ActionEvent event)
     	{
-    		// add score, reset frame, change player
+    		// send hand to scorer and add to player
+    		//reset round score
     		if (playerNumber == 4)
     		{
+    			// check end game conditions
     			playerNumber = 0;
     		}
     		playerNumber++;
@@ -216,7 +238,7 @@ public class mySideStuff {
     		
     		firstPanel.removeAll();
     		firstPanel.add(playerLabel, BorderLayout.NORTH);
-    		firstPanel.add(currentScore);
+    		firstPanel.add(currentScore); 
     		
     		buttPanel.removeAll();
     		
@@ -225,14 +247,54 @@ public class mySideStuff {
     			gameHand.setRolling(k, true);
     		}
     		gameHand.roll();
-			for (int k = 0; k < 6; k++)
+    		
+    		if (false) // add that check validity shit
 			{
-				JButton helper = new JButton("  " + gameHand.displayTheDiceValue(k) + "  ");
-				helper.addActionListener(new ColorAction(helper, k));
-				buttPanel.add(helper);
+				buttPanel.removeAll();
+				JLabel farkle = new JLabel("Get Farked Up");
+				buttPanel.add(farkle);
+				roundScore = 0;
+				buttPanel.repaint();
+				buttPanel.setVisible(true);
+				frame.setVisible(true);
+				endTurnAction.isFarkle(true);
+				playPanel.remove(0);
+			} else {
+				for (int k = 0; k < 6; k++)
+				{
+					JButton helper = new JButton("  " + gameHand.displayTheDiceValue(k) + "  ");
+					helper.addActionListener(new ColorAction(helper, k));
+					buttPanel.add(helper);
+				}
+			}
+			
+			if (didIFarkle == true)
+			{
+				playPanel.removeAll();
+				
+				JButton play = new JButton("Continue");
+				JButton endTurn = new JButton("End Turn");
+				
+				// making end turn button and keep playing button
+				playPanel.add(play);
+				playPanel.add(endTurn);
+				
+				panelAction player = new panelAction();
+				endTurnAction endIt = new endTurnAction();
+				
+				play.addActionListener(player);
+				endTurn.addActionListener(endIt);
+				
+				playPanel.repaint();
+				didIFarkle = false;
 			}
 			
 			frame.setVisible(true);
+    	}
+    	
+    	static public void isFarkle(Boolean didILose)
+    	{
+    		didIFarkle = didILose;
     	}
     }
 }
